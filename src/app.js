341b9a54773706e610e14a9f36aa9990f0011f49
aliaahmedraza <aliaahmedraza@gmail.com>
2025-04-21 12:49:59 +0500

    I am remove the cors error

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 const corsOptions = {
-  origin: "https://a-amart.vercel.app",
+  origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 };
